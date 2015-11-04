2c2a466f90b992356aabbed2f0a62239cddb017a
Karol Majta <karol.majta@sigmapoint.pl>
2015-11-04 11:22:04 +0100

    Fixed missing immstruct dependency

--- dist/index.js
+++ dist/index.js
@@ -1,42 +1,21 @@
 'use strict';
 
-var I = require('immutable');
 var immstruct = require('immstruct');
-var bacon = require('baconjs');
 
 var Bus = require('./Bus.js');
 var Dispatcher = require('./Dispatcher.js');
 
-var db = immstruct(I.Map([['users', I.Set([I.fromJS({ name: 'Mike Jordan', job: 'NBA Player' }), I.fromJS({ name: 'John Wayne', job: 'Cowboy' })])]]));
-
-// ----------------------------------------------------------------------------
-
-var dispatcher = Dispatcher();
-var bus = Bus(dispatcher.dispatch, function (h) {
-  return db.cursor().update(h);
-});
-
-dispatcher.registerHandler('users.add', function (db, event) {
-  db = db.update('users', function (users) {
-    return users.add(event[1]);
+var ReFrame = function ReFrame(initialDb) {
+  var db = immstruct(initialDb);
+  var dispatcher = Dispatcher();
+  var bus = Bus(dispatcher.dispatch, function (h) {
+    return db.cursor().update(h);
   });
-  bus.put(['report']);
-  return db;
-});
 
-dispatcher.registerHandler('users.remove', function (db, event) {
-  db = db.update('users', function (users) {
-    return users.remove(event[1]);
-  });
-  bus.put(['report']);
-  return db;
-});
+  return {
+    bus: bus,
+    dispatcher: dispatcher
+  };
+};
 
-bus.put(['users.add', I.fromJS({ name: 'Michael Jackson', job: 'Artist' })]);
-bus.put(['users.add', I.fromJS({ name: 'Mike Jordan', job: 'NBA Player' })]);
-bus.put(['users.add', I.fromJS({ name: 'Bob Dylan', job: 'Songwriter' })]);
-bus.put(['users.remove', I.fromJS({ name: 'Mike Jordan', job: 'NBA Player' })]);
-bus.put(['users.remove', I.fromJS({ name: 'ET', job: 'Alien' })]);
-bus.putSync(['test']);
-
-// ----------------------------------------------------------------------------
+module.exports = ReFrame;
